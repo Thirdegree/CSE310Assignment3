b91eaeb5280881bf1d5348790b033d360869c8e8
Joel Christiansen <greenbadge.jc@gmail.com>
2016-02-15 13:59:14 -0700

    initial commit

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,62 @@
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Calculator init
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return returns the current total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value The number to be added to the current total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value The number to be subtracted from the current total.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value The number to be multiplied by the current total.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value The number to divide the current total by.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return The history of operations as a string.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
